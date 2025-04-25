405801d3731ccda4b66a193f7cb3fc59d5e53f3c
rujh <rujhaan01@gmail.com>
2025-04-25 13:44:06 -0500

    added type declaration for node-quickbooks

--- src/types/node-quickbooks.d.ts
+++ src/types/node-quickbooks.d.ts
@@ -1,5 +1,23 @@
 declare module 'node-quickbooks' {
-    const QuickBooks: any;
-    export = QuickBooks;
+    export default class QuickBooks {
+      constructor(
+        consumerKey: string,
+        consumerSecret: string,
+        token: string,
+        tokenSecret: string | boolean,
+        realmId: string,
+        useSandbox: boolean,
+        debug: boolean,
+        minorversion: string | null,
+        oauthVersion: string,
+        refreshToken: string
+      );
+  
+      // Add only what you use — here's a basic example:
+      findAccounts?: (callback: Function) => void;
+      getCustomer?: (id: string, callback: Function) => void;
+      // or use index signature:
+      [key: string]: any;
+    }
   }
   
